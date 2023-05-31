326c1776e048cd703c109310adf2939a169e1331
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-05-31 05:01:22 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,38 +1,66 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
-import { Overlay, ModalImageBox} from './Modal.styled';
+import { Overlay, ModalImageBox } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleCloseByEsc);
-  }
+const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    const handleCloseByEsc = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleCloseByEsc);
-  }
+    window.addEventListener('keydown', handleCloseByEsc);
 
-  handleCloseByEsc = event => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+    return () => {
+      window.removeEventListener('keydown', handleCloseByEsc);
+    };
+  }, [onClose]);
 
-  handleBackdropClick = event => {
+  const handleBackdropClick = event => {
     if (event.target === event.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <Overlay onClick={this.handleBackdropClick}>
-        <ModalImageBox>{this.props.children}</ModalImageBox>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      <ModalImageBox>{children}</ModalImageBox>
+    </Overlay>,
+    modalRoot
+  );
+};
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleCloseByEsc);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleCloseByEsc);
+//   }
+
+//   handleCloseByEsc = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropClick = event => {
+//     if (event.target === event.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <Overlay onClick={this.handleBackdropClick}>
+//         <ModalImageBox>{this.props.children}</ModalImageBox>
+//       </Overlay>,
+//       modalRoot
+//     );
+//   }
+// }
 
 export { Modal };
